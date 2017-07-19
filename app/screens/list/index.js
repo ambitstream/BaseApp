ca7e3c6521f0d0c528f27d047388270c8319d77e
Alexander Kotliarov <slicer256@gmail.com>
2017-07-20 00:04:59 +0300

    list all bases names from hardcoded data

--- app/screens/list/index.js
+++ app/screens/list/index.js
@@ -4,31 +4,37 @@ import { Actions as NavActions } from 'react-native-router-flux';
 
 //Styles
 import { AppStyles } from '../../theme';
+import data from '../../data/bases'; 
 
 export default class ScreenComponent extends Component {
 
-  render() {
-    return (
-      <View style={AppStyles.markup.container}>
-        {this.props.isFavorites
-          ? <Text>There is no favorites bases</Text>
-          : <View>
-              <Text>This is list of bases:</Text>
-              <TouchableOpacity
-                onPress={()=>NavActions.single({ id: 1 })}>
-                <Text style={AppStyles.typo.link}>Base #1</Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                onPress={()=>NavActions.single({ id: 2 })}>
-                <Text style={AppStyles.typo.link}>Base #2</Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                onPress={()=>NavActions.single({ id: 3 })}>
-                <Text style={AppStyles.typo.link}>Base #3</Text>
-              </TouchableOpacity>
-            </View>
-        }
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View style={AppStyles.markup.container}>
+				{this.props.isFavorites
+					? <Text>There is no favorites bases</Text>
+					: <View>
+					
+						<Text>This is list of bases:</Text>
+						
+						<TouchableOpacity
+							onPress={()=>NavActions.single({ id: 1 })}>
+							<Text style={AppStyles.typo.link}>Base #1</Text>
+						</TouchableOpacity>
+						
+						{Object.keys(data).map(id => {
+							return(
+								<Text key={'base_' + id}> 
+									{data[id].title} 
+								</Text>
+							)
+						})}
+						
+						
+						
+					</View>
+				}
+			</View>
+		);
+	}
 }
