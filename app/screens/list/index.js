677e5579262bc4792223253bb6f27928fdc0ff31
Alexander Kotliarov <slicer256@gmail.com>
2017-07-21 20:21:31 +0300

    added comments icon to list

--- app/screens/list/index.js
+++ app/screens/list/index.js
@@ -14,10 +14,16 @@ class BaseItem extends Component {
 				onPress={()=>NavActions.single({ id: this.props.id })}
 			>
 
-				<View style={{flexDirection: 'row', flex: 1, height: 60}}>
+				<View style={{flexDirection: 'row', flex: 1, height: 70, borderBottomWidth: 1, borderBottomColor: 'lightgrey', marginTop: 5, marginBottom: 5}}>
 
-					<View>
-						<Image style={{flex: 1 }} source={{ uri: this.props.images[0] }}></Image>
+					<View style={{width:70, height:60}}>
+						<Text>
+							*****
+						</Text>
+						<View style={{flexDirection: 'row'}}>
+							{this.props.comments_count ? <Image style={{width:20, height:20, marginRight: 4}} source={Images.comments} /> : <Text/> }
+							<Text style={{fontWeight: 'bold'}}>{this.props.comments_count}</Text>
+						</View>
 					</View>
 
 					<View style={{flex: 2}}>
@@ -27,6 +33,10 @@ class BaseItem extends Component {
 						<Text style={{flex: 2, fontSize: 11}}>
 							{this.props.address}
 						</Text>
+					</View>
+					
+					<View style={{width: 100, height: 60, backgroundColor: 'skyblue'}}>
+						<Image style={{flex: 1 }} source={{ uri: this.props.images[0] }}></Image>
 					</View>
 
 				</View>
--- app/theme/images.js
+++ app/theme/images.js
@@ -1,6 +1,7 @@
 const Images = {
   splash: require('../assets/images/splash.png'),
   logo: require('../assets/images/logo.png'),
+  comments: require('../assets/icons/chat.png'),
   //splashBackground: require('../assets/images/splash-background.png'),
 }
 
