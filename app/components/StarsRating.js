81387189915d047c7b3e5230d255b67fd9b3b6f4
Alexander Kotliarov <slicer256@gmail.com>
2017-07-24 00:07:14 +0300

    properly display starsRating

--- app/components/StarsRating.js
+++ app/components/StarsRating.js
@@ -9,21 +9,35 @@ export default class StarsRating extends Component {
 	
 	constructor(props) {
 		super(props);		
-		this.starSize = props.starSize || 12;		
+		this.starSize = props.starSize || 12;
+		this.rating = props.rating || 0;
 	}
 	
 	render() {
 		
 		let stars = [];
+		
 		for (let i=1; i<=5; i++) {
-			stars.push('star');
+			if (this.rating < i) {
+				if (this.rating >= i - 0.5) {
+					stars.push('star_half');
+				}
+				else {
+					stars.push('star');
+				}
+			}
+			else {
+				stars.push('star_full');
+			}
+			
 		}
-		// 
-		Reactotron.log(stars);
 		
 		return (
 			<View style={{flex: 1, flexDirection: 'row'}}>
-				{stars.map(star_type => <Image source={Images.icons[star_type]} style={{width: this.starSize, height: this.starSize}}/> )}
+				{stars.map(star_type => <Image 
+					source={Images.icons[star_type]} 
+					style={{width: this.starSize, height: this.starSize}}
+				/> )}
 			</View>
 		)
 	}
