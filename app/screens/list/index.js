b01519bcd0dc11ee795035c270c1205f46dae855
Alexander Kotliarov <slicer256@gmail.com>
2017-07-27 23:37:02 +0300

    working quick search

--- app/screens/list/index.js
+++ app/screens/list/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Text, View, ListView } from 'react-native';
+import { Text, View, ListView, TextInput } from 'react-native';
+import Reactotron from 'reactotron-react-native';
 
 //Styles
 import { AppStyles} from '../../theme';
@@ -14,15 +15,28 @@ export default class ScreenComponent extends Component {
 
 	constructor() {
 		super();
-		const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+		this.ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 		this.state = {
-			dataSource: ds.cloneWithRows(Object.keys(data)),
+			dataSource: this.ds.cloneWithRows(Object.keys(data)),
+			searchText: '',
 		};
+	}
+	
+	searchChange(text) {
+		this.setState({
+			text,
+			dataSource: this.ds.cloneWithRows(Object.keys(data).filter( v => {
+				return data[v].title.indexOf(text) !== -1 || data[v].address.indexOf(text) !== -1
+			}))
+		});
 	}
 
 	render() {
 		return (
 			<View style={AppStyles.markup.container}>
+			
+				<TextInput style={{height: 30}} onChangeText={this.searchChange.bind(this)} placeholder="Поиск..."/>
+				
 				{this.props.isFavorites
 					? <View style={AppStyles.markup.commonPadding}>
 							<Text>There is no favorites bases</Text>
