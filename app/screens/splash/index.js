3c4084f94e093a1efe3d677ce02d148238c9905c
Eugene Kholyavkin <eugene.kholyavkin@gmail.com>
2017-08-01 11:55:48 +0300

    remove link from spalsh screen

--- app/screens/splash/index.js
+++ app/screens/splash/index.js
@@ -64,25 +64,23 @@ class ScreenComponent extends Component {
 
 	render() {
 		return (
-			<TouchableWithoutFeedback onPress={()=>NavActions.tabbar()}>
-				<View style={AppStyles.markup.containerSplash}>
+			<View style={AppStyles.markup.containerSplash}>
 
-					<View style={{position: 'absolute', top: 0, bottom: 0, left: 0, right: 0}}>
-						<Image source={require('../../assets/images/splash-background.jpg')} style={{flex: 1, width: null, height: null, resizeMode:'stretch'}} />
-					</View>
-
-					<ActivityIndicator />
-
-					<Anim>
-						<Image source={Images.logo} style={{width:266, height: 34, marginBottom: 10, marginTop: 20}} />
-					</Anim>
-
-					<Text style={{color: '#fff'}}>
-						Карта репетиционных баз
-					</Text>
-
+				<View style={{position: 'absolute', top: 0, bottom: 0, left: 0, right: 0}}>
+					<Image source={require('../../assets/images/splash-background.jpg')} style={{flex: 1, width: null, height: null, resizeMode:'stretch'}} />
 				</View>
-			</TouchableWithoutFeedback>
+
+				<ActivityIndicator />
+
+				<Anim>
+					<Image source={Images.logo} style={{width:266, height: 34, marginBottom: 10, marginTop: 20}} />
+				</Anim>
+
+				<Text style={{color: '#fff'}}>
+					Карта репетиционных баз
+				</Text>
+
+			</View>
 		);
 	}
 }
