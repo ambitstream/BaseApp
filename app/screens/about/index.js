ae941d39a14acb235511e7a0f125c067e1c12c6e
Alexander Kotliarov <slicer256@gmail.com>
2017-08-14 23:22:57 +0300

    added simple text for about page

--- app/screens/about/index.js
+++ app/screens/about/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
 import { connect } from 'react-redux';
 
 //Styles
@@ -9,9 +9,15 @@ class ScreenComponent extends Component {
 	render() {
 		return (
 			<View style={AppStyles.markup.container}>
-				<View style={AppStyles.markup.commonPadding}>
-					<Text>This is about page</Text>
-				</View>
+				<ScrollView style={AppStyles.markup.commonPadding}>
+					<Text>
+						Проект “Карта репетиционных баз” был создан музыкантами для музыкантов. Когда в Киеве количество репетиционных баз, как и начинающих групп, резко стало увеличиваться - искать и сравнивать их по форумам становилось все сложнее, а найти базу в месте, куда было бы удобно добираться всем участникам группы - только усложняло эту задачу. Поэтому было принято решение собрать всю информацию о репетиционных базах на одном сайте.
+					</Text>
+					<Text style={{marginTop: 10}}>
+						Так сайт увидел свет в 2009 году, куда были добавлены первые 20 баз. Позже, в 2017 году, было создано приложение для iOS и Android. 
+						Сейчас на сайте насчитывается более 100 баз с регулярными обновлениями и отзывами музыкантов.
+					</Text>
+				</ScrollView>
 			</View>
 		);
 	}
