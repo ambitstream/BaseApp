aa8aa6bb473a97421f40f32f377eab3d6299b3b6
Alex <slicer256@gmail.com>
2017-11-26 19:52:00 +0200

    added map markers to assets

--- app/theme/images.js
+++ app/theme/images.js
@@ -1,18 +1,22 @@
 const Images = {
-  splash: require('../assets/images/splash.png'),
-  logo: require('../assets/images/logo.png'),
-  comments: require('../assets/icons/chat.png'),
-  
-  icons: {
-	  star: require('../assets/icons/star.png'),
-	  star_half: require('../assets/icons/star_half.png'),
-	  star_full: require('../assets/icons/star_full.png'),
-	  map: require('../assets/icons/map.png'),
-	  user: require('../assets/icons/user.png'),
-	  list: require('../assets/icons/list.png'),
-	  search: require('../assets/icons/search.png'),
-  }
-  //splashBackground: require('../assets/images/splash-background.png'),
+    splash: require('../assets/images/splash.png'),
+    logo: require('../assets/images/logo.png'),
+    comments: require('../assets/icons/chat.png'),
+    
+    icons: {
+        star: require('../assets/icons/star.png'),
+        star_half: require('../assets/icons/star_half.png'),
+        star_full: require('../assets/icons/star_full.png'),
+        map: require('../assets/icons/map.png'),
+        user: require('../assets/icons/user.png'),
+        list: require('../assets/icons/list.png'),
+        search: require('../assets/icons/search.png'),
+        
+        marker_default: require('../assets/icons/marker-default.png'),
+        marker_vip: require('../assets/icons/marker-vip.png'),
+        marker_grey: require('../assets/icons/marker-grey.png'),
+    }
+    //splashBackground: require('../assets/images/splash-background.png'),
 }
 
 export default Images;
